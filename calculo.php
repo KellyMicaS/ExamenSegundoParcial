d30228fdde586e24bd54fbc12fdf57c8e94ad3d5
Kelly S <kellysolanodl@gmail.com>
2025-07-09 11:29:20 -0500

    Examen 2do parcial

--- calculo.php
+++ calculo.php
@@ -1,24 +1,19 @@
 <?php
-//Declaración de variables e ingreso de información del formulario
 $a=$_POST["a"];
 $b=$_POST["b"];
 $c=$_POST["c"];
 $x1="";
 $x2="";
 
-//Calculo del discriminante para la ecuación
 $determinante= $b * $b -4 *$a * $c;
 
-//Se usa if con una doble condición para determinar si la operación es válida o no
 if ($a == 0 || $determinante < 0){
     print "La ecuación no tiene solución.";
 } else{
-    //Usamos un sqrt para sacar la raiz cuadrada de la ecuación
     $raiz= sqrt($determinante);
     $x1= ($b + $raiz)/(2* $a);
     $x2= ($b - $raiz)/(2* $a);
-    
-    //Usamos el print para imprimir los resultados del cálculo
+
     print "Los números ingresados son: <br>";
     print "A:" .$a. "<br>";
     print "B:" .$b. "<br>";
